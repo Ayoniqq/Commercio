2ae7fd5e1bc4faaace2761d08f80fa328838c7bd
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-11 09:31:28 +0100

    Commercio

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//ALL ROUTES
+//ALL LOGIC
+
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT: ${PORT}`);
 });
