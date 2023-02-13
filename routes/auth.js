3186c6af89c954e1c751dc8c0beefdd553f6b608
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-13 06:49:05 +0100

    Commercio update

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,13 @@ const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
-    //Create User
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
     isAdmin: req.body.isAdmin,
   });
   try {
-    const savedUser = await newUser.save(); //Save User
+    const savedUser = await newUser.save();
     res.status(200).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
