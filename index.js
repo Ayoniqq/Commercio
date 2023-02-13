243890144a5164de9617165d830474241fdeb4b2
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-13 17:17:26 +0100

    commercio update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require("./config/db");
 const PORT = process.env.PORT || 8080;
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const cryptoJs = require("crypto-js");
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const cryptoJs = require("crypto-js"); //Password encryption
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
-    isAdmin: req.body.isAdmin,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET),
+    // isAdmin: req.body.isAdmin,
   });
   try {
     const savedUser = await newUser.save();
