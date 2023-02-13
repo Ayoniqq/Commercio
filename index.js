01bc3aa23015568a2b81313657ef171c06469d24
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-13 06:43:11 +0100

    Commercio

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const dotenv = require("dotenv").config();
 const db = require("./config/db");
 const PORT = process.env.PORT || 8080;
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 //ALL ROUTES
 //ALL LOGIC
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute); //Default route & userRoute from user.js
 
 app.listen(PORT, () => {
