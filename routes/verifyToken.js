cd5afc7ce49d544eb8d9f79abf910f64601e2faa
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-14 17:30:05 +0100

    Commercio

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.header.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) res.status(400).send("Token not valid!");
       req.user = user;
