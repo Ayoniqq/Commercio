f5b3aa60d859fe672a177178602d09aef46c68e9
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-19 15:52:54 +0100

    Commercio

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 //UPDATE USER INFO
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -24,24 +28,36 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+//Delete User
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted");
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
+  }
+});
+
+//GET User
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 // TESTING ROUTES
-// router.get("/usertest", (req, res) => {
-//   res.send(`User route is being accesesed`);
-// });
+router.get("/usertest", (req, res) => {
+  res.send(`User route is being accesesed`);
+});
 
-// router.post("/userposttest", (req, res) => {
-//   const username = req.body.username;
-//   res.send("User post sent!");
-//   console.log(username);
-// });
+router.post("/userposttest", (req, res) => {
+  const username = req.body.username;
+  res.send("User post sent!");
+  console.log(username);
+});
 
 module.exports = router;
