5f0f42ee796eb8c4eccdefcf36cb5b8c4a0184d8
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-21 14:22:49 +0100

    Commercio update

---
+++ routes/product.js
@@ -0,0 +1,99 @@
+const router = require("express").Router();
+
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+//CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//UPDATE USER INFO
+router.put("/product/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    //Update User
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.user.id,
+      { $set: req.body },
+      { new: true }
+    );
+    updatedProduct.save();
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Delete Product
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //GET ONE User
+router.get("/find/:id", async (req, res) => {
+  //We omitted verifyTokenAndAdmin here because everyboy should be allowed to view products
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //GET ALL Users
+// router.get("/", verifyTokenAndAdmin, async (req, res) => {
+//   const query = req.query.new;
+//   try {
+//     const users = query
+//       ? await User.find().sort({ _id: -1 }).limit(3) //returns the latest user(s) if query is true
+//       : await User.find(); //returns all users if query is false
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// //GET USER STATS (Statistics showing when users visited the platform)
+// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+//   const date = new Date();
+//   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1)); //Last year data
+//   try {
+//     //Getting the month and the number of users that visited
+//     const data = await User.aggregate([
+//       { $match: { createdAt: { $gte: lastYear } } },
+//       {
+//         $project: {
+//           month: { $month: "$createdAt" },
+//         },
+//       },
+//       {
+//         $group: {
+//           _id: "$month",
+//           total: { $sum: 1 },
+//         },
+//       },
+//     ]);
+//     res.status(200).json(data);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
