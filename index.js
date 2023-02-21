28245e3f4513e008f95d50b3c8abb48fb7d27aec
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-21 14:19:56 +0100

    Commercio

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const db = require("./config/db");
 const PORT = process.env.PORT || 8080;
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 app.use(express.json());
 app.use("/api/auth", authRoute); //authRoute
 app.use("/api/users", userRoute); //Default route & userRoute from user.js
+app.use("/api/products", productRoute); //productRoute
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT: ${PORT}`);
