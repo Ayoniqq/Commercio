d99329bd6f3ab4a993c10c0e7cfa1314597f786e
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-24 13:52:33 +0100

    commercio

--- routes/cart.js
+++ routes/cart.js
@@ -7,82 +7,82 @@ const {
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
-//CREATE PRODUCT
-router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = new Product(req.body);
+//CREATE CART
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
 
   try {
-    const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
 //UPDATE USER INFO
-router.put("/product/:id", verifyTokenAndAdmin, async (req, res) => {
+router.put("/cart/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     //Update User
-    const updatedProduct = await Product.findByIdAndUpdate(
+    const updatedCart = await Cart.findByIdAndUpdate(
       req.user.id,
       { $set: req.body },
       { new: true }
     );
-    updatedProduct.save();
-    res.status(200).json(updatedProduct);
+    updatedCart.save();
+    res.status(200).json(updatedCart);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//Delete Product
+// //Delete Product
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json("Product has been deleted");
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// //GET ONE Product
-router.get("/find/:id", async (req, res) => {
+//GET ONE Product /* Note that is User id not cart id */
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
   //We omitted verifyTokenAndAdmin here because everyboy should be allowed to view products
   try {
-    const product = await Product.findById(req.params.id);
-    res.status(200).json(product);
+    const cart = await Cart.findOne();
+    res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// //GET ALL Products
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  const qNew = req.query.new; //New Query
-  const qCategory = req.query.category; //Selected Category
-  try {
-    console.log("FETCHING");
-    if (qNew) {
-      console.log("NEW");
-      products = await Product.find().sort({ createdAt: -1 }).limit(2); //fetch most recently added products
-      res.status(200).json(products);
-    } else if (qCategory) {
-      console.log(qCategory);
-      const product = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      }); //Fetch products according to Category
-      res.status(200).json(product);
-    } else {
-      console.log("ALL PRODUCTS ARE DISPLAYED HERE");
-      const products = await Product.find();
-      res.status(200).json(products);
-    }
-    //res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// // //GET ALL Products
+// router.get("/", verifyTokenAndAdmin, async (req, res) => {
+//   const qNew = req.query.new; //New Query
+//   const qCategory = req.query.category; //Selected Category
+//   try {
+//     console.log("FETCHING");
+//     if (qNew) {
+//       console.log("NEW");
+//       products = await Product.find().sort({ createdAt: -1 }).limit(2); //fetch most recently added products
+//       res.status(200).json(products);
+//     } else if (qCategory) {
+//       console.log(qCategory);
+//       const product = await Product.find({
+//         categories: {
+//           $in: [qCategory],
+//         },
+//       }); //Fetch products according to Category
+//       res.status(200).json(product);
+//     } else {
+//       console.log("ALL PRODUCTS ARE DISPLAYED HERE");
+//       const products = await Product.find();
+//       res.status(200).json(products);
+//     }
+//     //res.status(200).json(users);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
