7e348b1a5c2f8c86a2d70fbcadf65873c710038a
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-24 07:56:38 +0100

    Commercio

--- routes/product.js
+++ routes/product.js
@@ -66,7 +66,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
       res.status(200).json(products);
     }
     elseif(qCategory) {
-      const product = await Product.find({}); //Fetch products according to Produc
+      const product = await Product.find({}); //Fetch products according to Category
     }
     res.status(200).json(users);
   } catch (err) {
