601d46c1c7f59d2207c69b70f5ced7aeeef5405c
Ayoniqq <ayorindeadeshina@gmail.com>
2023-02-28 14:13:34 +0100

    Commercio

--- routes/order.js
+++ routes/order.js
@@ -65,10 +65,21 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-router.get("/income", verifyTokenAndAdmin, (req, res) => {
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
-  const lastMonth = new Date(date.setMonth(date.getMonth) - 1);
-  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth) - 1);
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "createdAt" },
+        },
+      },
+    ]);
+  } catch (err) {}
 });
 
 module.exports = router;
